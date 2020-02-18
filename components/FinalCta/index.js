b703ec0e2499aded9e763c3ca70bd756c9a6a889
Stefano Verna <s.verna@datocms.com>
2020-02-18 13:06:38 +0100

    Preload fonts

--- components/FinalCta/index.js
+++ components/FinalCta/index.js
@@ -7,25 +7,23 @@ import s from './style.css';
 export default function FinalCta() {
   return (
     <Wrapper>
-      <Link href="/">
-        <a className={s.root}>
-          <div className={s.rootInner}>
-            <div className={s.body}>
-              <div className={s.title}>Start using DatoCMS today</div>
-              <div className={s.subtitle}>
-                According to Gartner 89% of companies plan to compete primarily
-                on the basis of customer experience this year. Don't get caught
-                unprepared.
-              </div>
-            </div>
-            <div className={s.action}>
-              <Checks checks={['No credit card', 'Easy setup']}>
-                <Button fs="big">Try it for free!</Button>
-              </Checks>
+      <a href="https://dashboard.datocms.com/signup" className={s.root}>
+        <div className={s.rootInner}>
+          <div className={s.body}>
+            <div className={s.title}>Start using DatoCMS today</div>
+            <div className={s.subtitle}>
+              According to Gartner 89% of companies plan to compete primarily on
+              the basis of customer experience this year. Don't get caught
+              unprepared.
             </div>
           </div>
-        </a>
-      </Link>
+          <div className={s.action}>
+            <Checks checks={['No credit card', 'Easy setup']}>
+              <Button fs="big">Try it for free!</Button>
+            </Checks>
+          </div>
+        </div>
+      </a>
     </Wrapper>
   );
 }
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,18 @@
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
+import Head from 'next/head';
 import './style.css';
 
 export default function Layout({ startNavbarHidden, children }) {
   return (
     <>
+      <Head>
+        <link rel="preload" href="/fonts/colfax-web-700.woff2" />
+        <link rel="preload" href="/fonts/colfax-web-bold.woff2" />
+        <link rel="preload" href="/fonts/tiempos-text-web-regular.woff2" />
+        <link rel="preload" href="/fonts/tiempos-headline-web-semibold.woff2" />
+        <link rel="preload" href="/fonts/colfax-web-regular.woff2" />
+      </Head>
       {!startNavbarHidden && <Navbar />}
       {children}
       <Footer />
