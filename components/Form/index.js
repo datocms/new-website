92c14080a6ecf1ddc4eb3381132243b9000e5202
Stefano Verna <s.verna@datocms.com>
2020-03-03 08:30:00 +0100

    Refactor

---
+++ components/Form/index.js
@@ -0,0 +1,150 @@
+import s from './style.css';
+import Button from 'components/Button';
+import Link from 'next/link';
+import {
+  FormContext,
+  useForm,
+  useFormContext,
+  Controller,
+} from 'react-hook-form';
+import cn from 'classnames';
+import { ToastProvider, useToasts } from 'react-toast-notifications';
+import { useEffect } from 'react';
+
+export const Field = ({
+  name,
+  label,
+  placeholder,
+  validations,
+  options,
+  as,
+}) => {
+  const { register, control, watch, errors } = useFormContext();
+  const ref = register(validations);
+  const value = watch(name);
+
+  let input = (
+    <input name={name} id={name} placeholder={placeholder} ref={ref} />
+  );
+
+  if (options) {
+    input = (
+      <>
+        {!value && (
+          <div className={s.selectPlaceholder}>Please select one...</div>
+        )}
+        <select name={name} id={name} ref={ref}>
+          <option value=""></option>
+          {options.map(option => {
+            const value = typeof option === 'string' ? option : option.value;
+            const label = typeof option === 'string' ? option : option.label;
+            return (
+              <option key={value} value={value}>
+                {label}
+              </option>
+            );
+          })}
+        </select>
+      </>
+    );
+  }
+
+  if (as) {
+    input = (
+      <Controller
+        as={as}
+        name={name}
+        id={name}
+        control={control}
+        placeholder={placeholder}
+        rules={validations}
+      />
+    );
+  }
+
+  return (
+    <div
+      className={cn(s.field, {
+        [s.fieldError]: errors[name] && errors[name].message,
+      })}
+    >
+      <label htmlFor={name}>{label}</label>
+
+      {errors[name] && <div className={s.error}>← {errors[name].message}</div>}
+      {input}
+    </div>
+  );
+};
+
+export function FormInner({ children, defaultValues, action, submitLabel }) {
+  const { addToast } = useToasts();
+
+  useEffect(() => {
+    var urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has('code')) {
+      const code = urlParams.get('code');
+
+      if (code === 'ok') {
+        addToast('Thank you! We will get in touch as soon as possible.', {
+          appearance: 'success',
+          autoDismiss: true,
+        });
+      } else {
+        addToast('Ouch! There was an error submitting the form!', {
+          appearance: 'error',
+          autoDismiss: true,
+        });
+      }
+    }
+  }, []);
+
+  const methods = useForm({
+    defaultValues,
+  });
+  const { handleSubmit } = methods;
+
+  const onSubmit = (values, event) => {
+    event.nativeEvent.currentTarget.submit();
+  };
+
+  return (
+    <FormContext {...methods}>
+      <form
+        className={s.form}
+        onSubmit={handleSubmit(onSubmit)}
+        method="POST"
+        action={action}
+        encType="multipart/form-data"
+        acceptCharset="utf-8"
+      >
+        {children}
+
+        <div className={s.submit}>
+          <div className={s.agree}>
+            By submitting you agree to our{' '}
+            <Link href="/legal/terms">
+              <a>TOS</a>
+            </Link>{' '}
+            and acknowledge our{' '}
+            <Link href="/legal/privacy-policy">
+              <a>Privacy Policy</a>
+            </Link>
+          </div>
+
+          <Button as="button" type="submit">
+            {submitLabel}
+          </Button>
+        </div>
+      </form>
+    </FormContext>
+  );
+}
+
+export function Form(props) {
+  return (
+    <ToastProvider>
+      <FormInner {...props} />
+    </ToastProvider>
+  );
+}
--- components/TalkWithUs/index.js
+++ components/TalkWithUs/index.js
@@ -1,127 +1,27 @@
 import s from './style.css';
-import Button from 'components/Button';
-import Link from 'next/link';
-import {
-  FormContext,
-  useForm,
-  useFormContext,
-  Controller,
-} from 'react-hook-form';
 import Textarea from 'react-autosize-textarea';
 import { getData } from 'country-list';
-import cn from 'classnames';
-import { ToastProvider, useToasts } from 'react-toast-notifications';
-import { useEffect } from 'react';
+import { Form, Field } from 'components/Form';
 
-const Field = ({ name, label, placeholder, validations, options, as }) => {
-  const { register, control, watch, errors } = useFormContext();
-  const ref = register(validations);
-  const value = watch(name);
-
-  let input = (
-    <input name={name} id={name} placeholder={placeholder} ref={ref} />
-  );
-
-  if (options) {
-    input = (
-      <>
-        {!value && (
-          <div className={s.selectPlaceholder}>Please select one...</div>
-        )}
-        <select name={name} id={name} ref={ref}>
-          <option value=""></option>
-          {options.map(option => {
-            const value = typeof option === 'string' ? option : option.value;
-            const label = typeof option === 'string' ? option : option.label;
-            return (
-              <option key={value} value={value}>
-                {label}
-              </option>
-            );
-          })}
-        </select>
-      </>
-    );
-  }
-
-  if (as) {
-    input = (
-      <Controller
-        as={as}
-        name={name}
-        id={name}
-        control={control}
-        placeholder={placeholder}
-        rules={validations}
-      />
-    );
-  }
-
-  return (
-    <div
-      className={cn(s.field, {
-        [s.fieldError]: errors[name] && errors[name].message,
-      })}
-    >
-      <label htmlFor={name}>{label}</label>
-
-      {errors[name] && <div className={s.error}>← {errors[name].message}</div>}
-      {input}
-    </div>
-  );
-};
-
-function TalkWithUs() {
-  const { addToast } = useToasts();
-
-  useEffect(() => {
-    var urlParams = new URLSearchParams(window.location.search);
-
-    if (urlParams.has('code')) {
-      const code = urlParams.get('code');
-
-      if (code === 'ok') {
-        addToast('Thank you! We will get in touch as soon as possible.', {
-          appearance: 'success',
-          autoDismiss: true,
-        });
-      } else {
-        addToast('Ouch! There was an error submitting the form!', {
-          appearance: 'error',
-          autoDismiss: true,
-        });
-      }
-    }
-  }, []);
-
-  const methods = useForm({
-    defaultValues: {
-      name: '',
-      email: '',
-      phoneNumber: '',
-      companyName: '',
-      country: '',
-      jobTitle: '',
-      companyRevenue: '',
-      numberEmployees: '',
-      body: '',
-    },
-  });
-  const { handleSubmit } = methods;
-
-  const onSubmit = (values, event) => {
-    event.nativeEvent.currentTarget.submit();
+export default function TalkWithUs() {
+  const defaultValues = {
+    name: '',
+    email: '',
+    phoneNumber: '',
+    companyName: '',
+    country: '',
+    jobTitle: '',
+    companyRevenue: '',
+    numberEmployees: '',
+    body: '',
   };
 
   return (
-    <FormContext {...methods}>
-      <form
-        className={s.form}
-        onSubmit={handleSubmit(onSubmit)}
-        method="POST"
+    <div className={s.root}>
+      <Form
         action="https://webhook.frontapp.com/forms/f51dbf7c0379d350b50e/4GuYjvVpHX6Xqau-2EggC1eKeg0Iw_fMbehg2EbuLpRQARK6OetUIsAzCTs5-NdwQS_X02Qo1vdMMh6aNGLiySEIPM3EqvAkgNvPW-dQ6BdvbK4bXw1qwh3D2i5j"
-        encType="multipart/form-data"
-        acceptCharset="utf-8"
+        defaultValues={defaultValues}
+        submitLabel="Get in touch"
       >
         <div className={s.formCols}>
           <Field
@@ -189,12 +89,7 @@ function TalkWithUs() {
             name="numberEmployees"
             label="# of employees"
             validations={{ required: 'Required' }}
-            options={[
-              '1-20',
-              '21-100',
-              '101-1000',
-              '1000+',
-            ]}
+            options={['1-20', '21-100', '101-1000', '1000+']}
           />
           <Field
             name="companyRevenue"
@@ -216,31 +111,7 @@ function TalkWithUs() {
           validations={{ required: 'Required' }}
           as={<Textarea />}
         />
-        <div className={s.submit}>
-          <div className={s.agree}>
-            By submitting you agree to our{' '}
-            <Link href="/legal/terms">
-              <a>TOS</a>
-            </Link>{' '}
-            and acknowledge our{' '}
-            <Link href="/legal/privacy-policy">
-              <a>Privacy Policy</a>
-            </Link>
-          </div>
-
-          <Button as="button" type="submit">
-            Get in touch
-          </Button>
-        </div>
-      </form>
-    </FormContext>
-  );
-}
-
-export default function OuterTalkWithUs() {
-  return (
-    <ToastProvider>
-      <TalkWithUs />
-    </ToastProvider>
+      </Form>
+    </div>
   );
 }
