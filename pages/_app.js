432baa5155cfaa868aacbc0f8ba48da4c6d1123b
Stefano Verna <s.verna@datocms.com>
2020-03-25 18:29:28 +0100

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,31 @@
+import Router from 'next/router';
+
 import '../components/NProgress/style.css';
 import '../components/BaseLayout/global.css';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { initAnalytics } from '@pinjollist/next-with-analytics';
+
+const options = {
+  trackingCode: 'UA-22858312-5',
+  respectDNT: true,
+};
+
+const analyticsInstance = initAnalytics(options);
+
+export default class MyApp extends React.PureComponent {
+  componentDidMount() {
+    const { handleRouteChange } = analyticsInstance;
+    Router.events.on('routeChangeComplete', handleRouteChange);
+  }
+
+  componentWillUnmount() {
+    const { handleRouteChange } = analyticsInstance;
+    Router.events.off('routeChangeComplete', handleRouteChange);
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return <Component {...pageProps} />;
+  }
 }
