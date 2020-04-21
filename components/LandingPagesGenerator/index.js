da00e1879b4f7404def34662e833823118d1787f
Stefano Verna <s.verna@datocms.com>
2020-04-21 15:53:37 +0200

    react-spring

--- components/LandingPagesGenerator/index.js
+++ components/LandingPagesGenerator/index.js
@@ -40,25 +40,44 @@ export default function UseModularBlocks() {
 
   let height = 0;
 
-  rows.forEach((row) => {
-    row.y = height;
-    height += row.height;
-  });
+  const transitions = useTransition(
+    rows.map((data) => ({ ...data, y: (height += data.height) - data.height })),
+    (d) => d.name,
+    {
+      from: { height: 0, opacity: 0 },
+      leave: { height: 0, opacity: 0 },
+      enter: ({ y, height }) => ({ y, height, opacity: 1 }),
+      update: ({ y, height }) => ({ y, height }),
+    },
+  );
 
-  const transitions = useTransition(rows, {
-    from: { height: 0, opacity: 0 },
-    leave: { height: 0, opacity: 0 },
-    enter: ({ y, height }) => ({ y, height, opacity: 1 }),
-    update: ({ y, height }) => ({ y, height }),
-  });
+  // rows.forEach((row) => {
+  //   row.y = height;
+  //   height += row.height;
+  // });
+
+  // const transitions = useTransition(rows, {
+  //   from: { height: 0, opacity: 0 },
+  //   leave: { height: 0, opacity: 0 },
+  //   enter: ({ y, height }) => ({ y, height, opacity: 1 }),
+  //   update: ({ y, height }) => ({ y, height }),
+  // });
 
   return (
     <UIChrome>
       <div className={s.body}>
         <div className={s.title}>Acme Inc.</div>
         <div className={s.list} style={{ height }}>
-          {transitions((style, item) => (
-            <animated.div className={s.card} style={style}>
+          {transitions.map(({ item, props: { y, ...rest }, key }, index) => (
+            <animated.div
+              key={key}
+              className={s.card}
+              style={{
+                zIndex: blocks.length - index,
+                transform: y.interpolate((y) => `translate3d(0,${y}px,0)`),
+                ...rest,
+              }}
+            >
               <div className={s.cell}>
                 <div
                   className={s.details}
@@ -67,6 +86,16 @@ export default function UseModularBlocks() {
               </div>
             </animated.div>
           ))}
+          {/* {transitions((style, item) => (
+            <animated.div className={s.card} style={style}>
+              <div className={s.cell}>
+                <div
+                  className={s.details}
+                  style={{ backgroundImage: item.css }}
+                />
+              </div>
+            </animated.div>
+          ))} */}
         </div>
       </div>
     </UIChrome>
