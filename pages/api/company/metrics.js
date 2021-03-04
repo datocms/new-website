5a2958e10aba9cc81368652beddcd266fa227c84
Stefano Verna <s.verna@datocms.com>
2021-03-04 18:22:40 +0100

    Whoops

--- pages/api/company/metrics.js
+++ pages/api/company/metrics.js
@@ -2,7 +2,7 @@ import tiny from 'tiny-json-http';
 
 export default async (_req, res) => {
   const { body: data } = await tiny.get({
-    url: `https://api.profitwell.com/v2/metrics/monthly/?metrics=recurring_revenue,average_revenue_per_user,active_customers`,
+    url: `https://api.profitwell.com/v2/metrics/monthly/?metrics=recurring_revenue`,
     headers: {
       Authorization: process.env.PROFITWELL_API_KEY,
     },
