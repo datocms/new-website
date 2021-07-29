a7c8f8d32695072261caff0b7393bbcfde47c5ce
Stefano Verna <s.verna@datocms.com>
2021-07-29 16:38:25 +0200

    Return page as JSON for easier consumption

--- pages/api/preview/getHtml.js
+++ pages/api/preview/getHtml.js
@@ -1,6 +1,9 @@
 import { SiteClient } from 'datocms-client';
 import got from 'got';
 
+// this "routing" function knows how to convert a DatoCMS record
+// into its canonical URL within the website
+
 const recordToUrl = async (record, model) => {
   switch (model.apiKey) {
     case 'blog_post':
@@ -15,6 +18,9 @@ const recordToUrl = async (record, model) => {
 };
 
 const handler = async (req, res) => {
+  // this endpoint requires an itemId parameter, which represent the
+  // ID of the record we want to get the preview for
+
   const itemId = req.query.itemId;
 
   if (!itemId) {
@@ -22,12 +28,17 @@ const handler = async (req, res) => {
     return;
   }
 
+  // given the itemId, we can now get the record (and it's model) from the Datocms API
+
   const client = new SiteClient(
     'faeb9172e232a75339242faafb9e56de8c8f13b735f7090964',
   );
 
   const record = await client.items.find(itemId);
   const model = await client.itemTypes.find(record.itemType);
+
+  // we can now get the URL for the record using a routing function
+  // that knows which record is linked to which URL in the website
 
   const url = await recordToUrl(record, model);
 
@@ -37,6 +48,8 @@ const handler = async (req, res) => {
     });
     return;
   }
+
+  // let's start a Next.js Preview Mode (and get the authentication cookies)
 
   const { headers } = await got(
     new URL('/api/preview/start', process.env.BASE_URL).toString(),
@@ -49,15 +62,18 @@ const handler = async (req, res) => {
     .map((cookie) => cookie.split(';')[0])
     .join(';');
 
-  const pageUrl = new URL(url, process.env.BASE_URL);
+  // final step is to get the HTML of the webpage associated with the record
+  // and return it to the client
 
-  const { body } = await got(pageUrl.toString(), { headers: { cookie } });
+  const { body } = await got(new URL(url, process.env.BASE_URL).toString(), {
+    headers: { cookie },
+  });
 
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-  res.status(200).send(body);
+  res.status(200).json({ body });
 };
 
 export default handler;
