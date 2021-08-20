19904bd771cbe3adc0d3f6714282ee12fdf4d80a
Stefano Verna <s.verna@datocms.com>
2021-08-20 11:31:35 +0200

    Fix videos on iOS

--- components/VideoPlayer/index.js
+++ components/VideoPlayer/index.js
@@ -5,7 +5,26 @@ export default function VideoPlayer({ src, autoPlay, ...other }) {
   const ref = useRef();
 
   useEffect(() => {
-    var hls = new Hls();
+    const videoEl = ref.current;
+
+    if (!videoEl) {
+      return;
+    }
+
+    if (videoEl.canPlayType('application/vnd.apple.mpegurl')) {
+      console.log('HLS supported natively!');
+      // This will run in safari, where HLS is supported natively
+      videoEl.src = src;
+      return;
+    }
+
+    if (!Hls.isSupported()) {
+      console.log('HLS not supported and HLS is not supported natively!');
+      return;
+    }
+
+    const hls = new Hls();
+
     hls.attachMedia(ref.current);
 
     hls.on(Hls.Events.MEDIA_ATTACHED, function () {
@@ -16,7 +35,19 @@ export default function VideoPlayer({ src, autoPlay, ...other }) {
         }
       });
     });
+
+    return () => {
+      if (hls) {
+        hls.destroy();
+      }
+    };
   }, [autoPlay, src]);
+
+  const poster =
+    other.poster ||
+    src
+      .replace('stream.mux.com', 'image.mux.com')
+      .replace('.m3u8', '/thumbnail.jpg?height=600');
 
   return (
     <video
@@ -24,6 +55,7 @@ export default function VideoPlayer({ src, autoPlay, ...other }) {
       muted={autoPlay}
       {...other}
       ref={ref}
+      poster={poster}
       style={{ display: 'block' }}
     />
   );
