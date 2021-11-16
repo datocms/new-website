714c417161efb9f569e0922edf1d7afd6c19eb60
Stefano Verna <s.verna@datocms.com>
2021-11-16 23:10:51 +0100

    Optimizations

--- components/BaseLayout/index.js
+++ components/BaseLayout/index.js
@@ -8,71 +8,6 @@ export default function Layout({ preview, children }) {
 
   return (
     <>
-      <Head>
-        <link rel="preload" href="/fonts/colfax-web-700.woff2" as="font" />
-        <link rel="preload" href="/fonts/colfax-web-bold.woff2" as="font" />
-        <link
-          rel="preload"
-          href="/fonts/tiempos-text-web-regular.woff2"
-          as="font"
-        />
-        <link
-          rel="preload"
-          href="/fonts/tiempos-headline-web-semibold.woff2"
-          as="font"
-        />
-        <link rel="preload" href="/fonts/colfax-web-regular.woff2" as="font" />
-        <meta
-          name="google-site-verification"
-          content="wfOsq57h3qCQUTbHcX-4qEEY07vgi4KgH9rdT1ywwwc"
-        />
-        {[16, 32, 96, 192].map((size) => (
-          <link
-            rel="icon"
-            sizes="16x16"
-            href={`https://www.datocms-assets.com/205/1525789775-dato.png?w=${size}&amp;h=${size}`}
-            type="image/png"
-            key={size}
-          />
-        ))}
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="DatoCMS Blog"
-          href="/blog.xml"
-        />
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="DatoCMS Product Changelog"
-          href="/product-changelog.xml"
-        />
-        <meta
-          property="og:image"
-          content="https://www.datocms-assets.com/205/1614353889-social.png?fit=max&amp;fm=jpg&amp;w=1000"
-          key="meta-og:image"
-        />
-        <meta
-          property="og:image:width"
-          content="1450"
-          key="meta-og:image:width"
-        />
-        <meta
-          property="og:image:height"
-          content="1452"
-          key="meta-og:image:height"
-        />
-        <meta
-          name="twitter:image"
-          key="meta-twitter:image"
-          content="https://www.datocms-assets.com/205/1614353889-social.png?fit=max&amp;fm=jpg&amp;w=1000"
-        />
-        <meta
-          key="meta-twitter:card"
-          name="twitter:card"
-          content="summary_large_image"
-        />
-      </Head>
       <NProgress />
       {preview && (
         <a
@@ -83,20 +18,6 @@ export default function Layout({ preview, children }) {
         </a>
       )}
       <div className={s.root}>{children}</div>
-
-      <div
-        className={s.overlay}
-        style={{
-          backgroundColor: 'white',
-          position: 'fixed',
-          transition: 'all 0.25s ease-in-out',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          zIndex: 2000,
-        }}
-      />
     </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,58 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="wfOsq57h3qCQUTbHcX-4qEEY07vgi4KgH9rdT1ywwwc"
+          />
+          {[16, 32, 96, 192].map((size) => (
+            <link
+              rel="icon"
+              sizes={`${size}x${size}`}
+              href={`https://www.datocms-assets.com/205/1525789775-dato.png?w=${size}&amp;h=${size}`}
+              type="image/png"
+              key={size}
+            />
+          ))}
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            title="DatoCMS Blog"
+            href="/blog.xml"
+          />
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            title="DatoCMS Product Changelog"
+            href="/product-changelog.xml"
+          />
+          <meta
+            property="og:image"
+            content="https://www.datocms-assets.com/205/1614353889-social.png?fit=max&amp;fm=jpg&amp;w=1000"
+            key="meta-og:image"
+          />
+          <meta
+            property="og:image:width"
+            content="1450"
+            key="meta-og:image:width"
+          />
+          <meta
+            property="og:image:height"
+            content="1452"
+            key="meta-og:image:height"
+          />
+          <meta
+            name="twitter:image"
+            key="meta-twitter:image"
+            content="https://www.datocms-assets.com/205/1614353889-social.png?fit=max&amp;fm=jpg&amp;w=1000"
+          />
+          <meta
+            key="meta-twitter:card"
+            name="twitter:card"
+            content="summary_large_image"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
