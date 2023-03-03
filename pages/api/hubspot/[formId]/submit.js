b928f7791d4e494d6a72bbe5065d109745d64fb9
Stefano Verna <s.verna@datocms.com>
2023-03-03 15:57:56 +0100

    Support CORS

--- pages/api/hubspot/[formId]/submit.js
+++ pages/api/hubspot/[formId]/submit.js
@@ -4,10 +4,16 @@ const rollbar = process.env.ROLLBAR_TOKEN
   ? new Rollbar(process.env.ROLLBAR_TOKEN)
   : { error: () => {} };
 
-const hubspotPortalId = '27002970';
-
 const handler = async (req, res) => {
   try {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
+    res.setHeader('Content-Type', 'application/json');
+
     if (req.method !== 'POST') {
       return res.status(404).send('Invalid endpoint!');
     }
@@ -18,10 +24,8 @@ const handler = async (req, res) => {
       return res.status(404).send('Missing form ID');
     }
 
-    console.log('Forwarded IP', req.headers['x-vercel-forwarded-for']);
-
     const hubspotRequest = await fetch(
-      `https://api.hsforms.com/submissions/v3/integration/secure/submit/${hubspotPortalId}/${formId}`,
+      `https://api.hsforms.com/submissions/v3/integration/secure/submit/${process.env.HUBSPOT_PORTAL_ID}/${formId}`,
       {
         method: 'POST',
         headers: {
