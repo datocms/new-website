da178e15dc8100cccd79774869298d96892e4f3b
arcataroger <arcataroger@gmail.com>
2024-01-26 16:28:31 -0800

    Revert "Colorize HTTP headers in requests and responses"
    This reverts commit 851965a8cd949851b24a70a3148d29bb302cdaf5.

--- components/Prism/index.js
+++ components/Prism/index.js
@@ -6,26 +6,19 @@ import s from './style.module.css';
 import { range } from 'range';
 import { useMemo } from 'react';
 
-const modifiedStyles = [
-  {
-    types: ['comment'],
-    style: {
-      color: '#98c6e7',
-      fontStyle: 'italic',
-    },
-  },
-  {
-    types: ['header-name'],
-    languages: ['http'],
-    style: {
-      color: 'rgb(130, 170, 255)',
-    },
-  },
-];
-
 const modifiedTheme = {
   ...theme,
-  styles: [...theme.styles, ...modifiedStyles],
+  styles: theme.styles.map((style) =>
+    style.types.includes('comment')
+      ? {
+          types: ['comment'],
+          style: {
+            color: '#98c6e7',
+            fontStyle: 'italic',
+          },
+        }
+      : style,
+  ),
 };
 
 const Prism = ({ code, language, showLineNumbers, highlightLines = [] }) => {
