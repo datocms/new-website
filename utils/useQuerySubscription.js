416e9bd463e4dcfceebd29ae5103902c932f101b
Silvano Stralla <silvano.stralla@sistrall.it>
2024-02-21 17:58:22 +0100

    Try a custom fetcher

--- utils/useQuerySubscription.js
+++ utils/useQuerySubscription.js
@@ -1,18 +1,25 @@
 import { useQuerySubscription as realUseQuerySubscription } from 'react-datocms';
 
+const fetcher = (baseUrl, { headers, method, body }) => {
+  return fetch(baseUrl, {
+    headers: {
+      ...headers,
+      ...(process.env.VERCEL_ENV === 'preview'
+        ? {
+            'X-Visual-Editing': 'vercel-v1',
+            'X-Base-Editing-Url': 'https://cms.datocms.com',
+          }
+        : {}),
+    },
+    method,
+    body,
+  });
+};
+
 export function useQuerySubscription(config) {
   return realUseQuerySubscription({
     ...config,
     environment: process.env.NEXT_PUBLIC_DATOCMS_ENVIRONMENT_ID,
-    fetcher: (baseUrl, { headers, method, body }) =>
-      fetch(baseUrl, {
-        headers: {
-          ...headers,
-          'X-Visual-Editing': 'vercel-v1',
-          'X-Base-Editing-Url': 'https://cms.datocms.com',
-        },
-        method,
-        body,
-      }),
+    fetcher,
   });
 }
