16db1a846eaf32f5d0fbdb3435291d4711647cca
Silvano Stralla <silvano.stralla@sistrall.it>
2024-02-21 11:34:38 +0100

    Inject visual editing headers in useQuerySubscription

--- utils/useQuerySubscription.js
+++ utils/useQuerySubscription.js
@@ -4,5 +4,19 @@ export function useQuerySubscription(config) {
   return realUseQuerySubscription({
     ...config,
     environment: process.env.NEXT_PUBLIC_DATOCMS_ENVIRONMENT_ID,
+    fetcher: (baseUrl, { headers, method, body }) =>
+      fetch(baseUrl, {
+        headers: {
+          ...headers,
+          ...(process.env.VERCEL_ENV === 'preview'
+            ? {
+                'X-Visual-Editing': 'vercel-v1',
+                'X-Base-Editing-Url': 'https://cms.datocms.com',
+              }
+            : {}),
+        },
+        method,
+        body,
+      }),
   });
 }
