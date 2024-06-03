4d650354c61a8c2fff1025aa8a0c9364242558a8
Marco Mezzavilla <hub@marcomezzavilla.com>
2024-06-03 10:10:06 +0200

    Clean up

--- pages/marketplace/index.jsx
+++ pages/marketplace/index.jsx
@@ -1,4 +1,3 @@
-import cn from 'classnames';
 import Head from 'components/Head';
 import MarketplaceCard from 'components/MarketplaceCard';
 import Layout from 'components/MarketplaceLayout';
@@ -119,18 +118,17 @@ export const getStaticProps = handleErrors(async ({ preview }) => {
 });
 
 const Category = ({ title, description, children, browse }) => (
-  <div className={s.categoryWrapper}>
-    <div className={s.category}>
-      <div className={s.categoryIntro}>
-        <div className={s.sectionHeader}>
-          <div className={s.headerTitle}>{title}</div>
-          {browse}
-        </div>
-        <div className={s.intro}>{description}</div>
+  <div className={s.category}>
+    <div className={s.categoryIntro}>
+      <div className={s.header}>
+        <div className={s.headerTitle}>{title}</div>
+        {browse}
       </div>
-
-      <div className={s.boxes}>{children}</div>
+      <div className={s.intro}>
+        <p>{description}</p>
+      </div>
     </div>
+    {children}
   </div>
 );
 
@@ -209,69 +207,67 @@ export default function IntegrationsPage({
         <title>Integrations Marketplace</title>
       </Head>
 
-      <div className={s.sectionWrapper}>
-        <section className={s.section}>
-          <div className={s.sectionHeader}>
-            <h2 className={s.headerTitle}>Our Starters</h2>
-            <Link href="/marketplace/starters">
-              <a className={s.headerViewAll}>
-                <span>View all</span> <ArrowRight />
-              </a>
-            </Link>
+      <Category
+        title="Our Starters"
+        browse={
+          <Link href="/marketplace/starters">
+            <a className={s.headerViewAll}>
+              <span>View all </span> <ArrowRight />
+            </a>
+          </Link>
+        }
+      >
+        <div className={s.startersWrapper}>
+          <div className={s.fullFledged}>
+            <div className={s.intro}>
+              <h3>Full-fledged demos</h3>
+              <p>
+                Use our pre-built demo projects to see all of DatoCMS&apos;s
+                features in a realistic production-ready setup. Includes many
+                example content types, and advanced features.
+              </p>
+            </div>
+            {fullFledged.map((item) => (
+              <MarketplaceCard
+                key={item.code}
+                href={`/marketplace/starters/${item.code}`}
+                image={item.screenshot.responsiveImage}
+                technology={item.technology}
+                text={{
+                  title: item.name,
+                  description: item.cmsDescription,
+                }}
+                highlight={'Best choice to start!'}
+                badge={item.badge}
+                label={item.label}
+                size="large"
+              />
+            ))}
           </div>
 
-          <div className={s.startersWrapper}>
-            <div className={s.fullFledged}>
-              <div className={s.intro}>
-                <h3>Full-fledged demos</h3>
-                <p>
-                  Use our pre-built demo projects to see all of DatoCMS&apos;s
-                  features in a realistic production-ready setup. Includes many
-                  example content types, and advanced features.
-                </p>
-              </div>
-              {fullFledged.map((item) => (
-                <MarketplaceCard
-                  key={item.code}
-                  href={`/marketplace/starters/${item.code}`}
-                  image={item.screenshot.responsiveImage}
-                  technology={item.technology}
-                  text={{
-                    title: item.name,
-                    description: item.cmsDescription,
-                  }}
-                  highlight={'Best choice to start!'}
-                  badge={item.badge}
-                  label={item.label}
-                  size="large"
-                />
-              ))}
+          <div className={s.techStarters}>
+            <div className={s.intro}>
+              <h3>Tech starters</h3>
+              <p>Kickstart your next project with our scaffolds.</p>
             </div>
 
-            <div className={s.techStarters}>
-              <div className={s.intro}>
-                <h3>Tech starters</h3>
-                <p>Kickstart your next project with our scaffolds.</p>
-              </div>
-
-              {techStarters.map((item) => (
-                <MarketplaceCard
-                  key={item.code}
-                  href={`/marketplace/starters/${item.code}`}
-                  technology={item.technology.squareLogo}
-                  text={{
-                    title: item.name,
-                    description: item.cmsDescription,
-                  }}
-                  boxed={false}
-                  orientation="horizontal"
-                  size="small"
-                />
-              ))}
-            </div>
+            {techStarters.map((item) => (
+              <MarketplaceCard
+                key={item.code}
+                href={`/marketplace/starters/${item.code}`}
+                technology={item.technology.squareLogo}
+                text={{
+                  title: item.name,
+                  description: item.cmsDescription,
+                }}
+                boxed={false}
+                orientation="horizontal"
+                size="small"
+              />
+            ))}
           </div>
-        </section>
-      </div>
+        </div>
+      </Category>
 
       <Category
         title="Community Plugins"
@@ -284,34 +280,31 @@ export default function IntegrationsPage({
         }
         description="Easily expand and customize the capabilities of DatoCMS with community plugins"
       >
-        <Carousel options={carouselOptions}>
-          {page.plugins.map((item, index) => (
-            <div key={index} className={s.emblaSlide}>
-              <MarketplaceCard
-                href={`/marketplace/plugins/${item.code}`}
-                image={item.coverImage.responsiveImage}
-                technology={item.technology}
-                text={{
-                  title: item.title,
-                  description: item.description,
-                }}
-                badge={item.badge}
-                label={item.label}
-                size="medium"
-              />
-            </div>
-          ))}
-        </Carousel>
+        <div className={s.carouselWrapper}>
+          <Carousel options={carouselOptions}>
+            {page.plugins.map((item, index) => (
+              <div key={index} className={s.emblaSlide}>
+                <MarketplaceCard
+                  href={`/marketplace/plugins/${item.code}`}
+                  image={item.coverImage.responsiveImage}
+                  technology={item.technology}
+                  text={{
+                    title: item.title,
+                    description: item.description,
+                  }}
+                  badge={item.badge}
+                  label={item.label}
+                  size="medium"
+                />
+              </div>
+            ))}
+          </Carousel>
+        </div>
       </Category>
 
       <Category
         title="Hosting &amp; CI Building"
-        description={
-          <>
-            Server, serverless or static: no matter the stack you&#39;re using,
-            we&#39;ve got you covered
-          </>
-        }
+        description="Server, serverless or static: no matter the stack you&#39;re using, we&#39;ve got you covered"
         browse={
           <Link href="/marketplace/hosting">
             <a className={s.headerViewAll}>
@@ -320,33 +313,30 @@ export default function IntegrationsPage({
           </Link>
         }
       >
-        <Carousel options={carouselOptions}>
-          {page.hostingBuilding.map((item, index) => (
-            <div key={index} className={s.emblaSlide}>
-              <MarketplaceCard
-                href={`/marketplace/hosting/${item.code}`}
-                technology={item.logo}
-                text={{
-                  title: item.title,
-                  description: item.description,
-                }}
-                badge={item.badge}
-                label={item.label}
-                size="medium"
-              />
-            </div>
-          ))}
-        </Carousel>
+        <div className={s.carouselWrapper}>
+          <Carousel options={carouselOptions}>
+            {page.hostingBuilding.map((item, index) => (
+              <div key={index} className={s.emblaSlide}>
+                <MarketplaceCard
+                  href={`/marketplace/hosting/${item.code}`}
+                  technology={item.logo}
+                  text={{
+                    title: item.title,
+                    description: item.description,
+                  }}
+                  badge={item.badge}
+                  label={item.label}
+                  size="medium"
+                />
+              </div>
+            ))}
+          </Carousel>
+        </div>
       </Category>
 
       <Category
         title={<>Enterprise Apps</>}
-        description={
-          <>
-            Keep your company data secure with centralized users management and
-            assets storage
-          </>
-        }
+        description="Keep your company data secure with centralized users management and assets storage"
         browse={
           <Link href="/marketplace/enterprise">
             <a className={s.headerViewAll}>
@@ -356,23 +346,25 @@ export default function IntegrationsPage({
           </Link>
         }
       >
-        <Carousel options={carouselOptions}>
-          {page.enterpriseApps.map((item, index) => (
-            <div key={index} className={s.emblaSlide}>
-              <MarketplaceCard
-                href={`/marketplace/enterprise/${item.code}`}
-                technology={item.logo}
-                text={{
-                  title: item.title,
-                  description: item.description,
-                }}
-                badge={item.badge}
-                label={item.label}
-                size="medium"
-              />
-            </div>
-          ))}
-        </Carousel>
+        <div className={s.carouselWrapper}>
+          <Carousel options={carouselOptions}>
+            {page.enterpriseApps.map((item, index) => (
+              <div key={index} className={s.emblaSlide}>
+                <MarketplaceCard
+                  href={`/marketplace/enterprise/${item.code}`}
+                  technology={item.logo}
+                  text={{
+                    title: item.title,
+                    description: item.description,
+                  }}
+                  badge={item.badge}
+                  label={item.label}
+                  size="medium"
+                />
+              </div>
+            ))}
+          </Carousel>
+        </div>
       </Category>
     </Layout>
   );
