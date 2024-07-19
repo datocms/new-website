74ceebecf494a0986d7ab088136617873ac51793
Marco Mezzavilla <hub@marcomezzavilla.com>
2024-07-19 09:12:18 +0200

    Update styles

--- pages/use-cases/[slug]/index.jsx
+++ pages/use-cases/[slug]/index.jsx
@@ -2,9 +2,14 @@ import cn from 'classnames';
 import Button from 'components/Button';
 import Head from 'components/Head';
 import Hero from 'components/Hero';
+import Highlight from 'components/Highlight';
 import { highlightStructuredText } from 'components/Highlight';
+import InterstitialTitle from 'components/InterstitialTitle';
 import Layout from 'components/Layout';
 import LazyImage from 'components/LazyImage';
+import QuotesCarousel from 'components/QuotesCarousel';
+import Space from 'components/Space';
+import TitleStripWithContent from 'components/TitleStripWithContent';
 import Wrapper from 'components/Wrapper';
 import {
   gqlStaticPaths,
@@ -14,6 +19,8 @@ import {
   seoMetaTagsFields,
 } from 'lib/datocms';
 import Link from 'next/link';
+import Arrow from 'public/images/arrow.svg';
+import Zen from 'public/images/illustrations/zen.svg';
 import {
   Image as DatoImage,
   StructuredText,
@@ -41,8 +48,17 @@ export const getStaticProps = gqlStaticPropsWithSubscription(
         seo: _seoMetaTags {
           ...seoMetaTagsFields
         }
+
         title {
           value
+        }
+        subtitle {
+          value
+        }
+        heroImage {
+          responsiveImage {
+            ...imageFields
+          }
         }
 
         quotesHeader: header {
@@ -147,85 +163,122 @@ export default function UseCase({ subscription, preview }) {
     data: { page },
   } = useQuerySubscription(subscription);
 
+  const quotes = page.quotes;
+
   return (
-    <Layout preview={preview}>
+    <Layout preview={preview} noCta>
       <Head>{renderMetaTags(page.seo)}</Head>
 
-      <Wrapper>
-        <Hero title={highlightStructuredText(page.title)} />
-      </Wrapper>
+      <div className={s.hero2}>
+        <Wrapper>
+          <div className={s.heroTitle}>
+            <Hero title={highlightStructuredText(page.title)} />
+          </div>
+
+          <div className={s.heroInfo}>
+            <div className={s.heroImage}>
+              <DatoImage data={page.heroImage.responsiveImage} />
+            </div>
+            <div className={s.heroText}>
+              {/* <h1 className={s.heroTitle}>{highlightStructuredText(page.title)}</h1> */}
+              <h3 className={s.heroSubtitle}>
+                {highlightStructuredText(page.subtitle)}
+              </h3>
+              <div className={s.buttonContainer}>
+                <Button as="a" href="https://dashboard.datocms.com/signup">
+                  Try it for free
+                </Button>
+                <Button as="a" s="invert" href="/contact">
+                  Contact sales
+                </Button>
+              </div>
+            </div>
+          </div>
+        </Wrapper>
+      </div>
 
       <div className={s.testimonials}>
         <h2 className={s.testimonialTitle}>
           <StructuredText data={page.quotesHeader} />
         </h2>
-        <div className={s.testimonialsContainer}>
-          {page.quotes
-            .filter((t) => t.quote)
-            .map((testimonial) => {
-              return (
-                <div key={testimonial.id} className={s.quoteWrapper}>
-                  <div className={s.quote}>
-                    {highlightStructuredText(testimonial.quote)}
-                  </div>
-                  <div className={s.info}>
-                    <DatoImage
-                      className={s.avatar}
-                      data={testimonial.image.responsiveImage}
-                    />
-                    {testimonial.partner ? (
-                      <Link
-                        href={`/partners/${testimonial.partner.slug}`}
-                        passHref
-                      >
-                        <div className={s.authorRole}>
-                          <div className={s.name}>{testimonial.name}</div>
-                          <div className={s.role}>
-                            {testimonial.role} @ {testimonial.partner.name}
-                          </div>
-                        </div>
-                      </Link>
-                    ) : (
-                      <div className={s.authorRole}>
-                        <div className={s.name}>{testimonial.name}</div>
-                        <div className={s.role}>{testimonial.role}</div>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              );
-            })}
-        </div>
+
+        <Space top={1} bottom={2}>
+          <QuotesCarousel quotes={quotes} animated={true} />
+        </Space>
       </div>
 
-      <div className={s.starter}>
-        <Wrapper>
+      <TitleStripWithContent
+        title={<StructuredText data={page.starterTitle} />}
+      >
+        <div className={s.starterBox}>
           <div className={s.starterText}>
-            <h2>
-              <StructuredText data={page.starterTitle} />
-            </h2>
             <StructuredText data={page.starterDescription} />
             <Button as="a" p="small" href={page.starterLink}>
               Check it out
             </Button>
           </div>
-        </Wrapper>
 
-        {page.starterImage?.responsiveImage && (
-          <div className={s.starterImage}>
-            <div className={s.imageWrapper}>
-              <DatoImage
-                className={s.test}
-                data={page.starterImage.responsiveImage}
-                pictureStyle={{ objectFit: 'contain', objectPosition: 'left' }}
-              />
+          {page.starterImage?.responsiveImage && (
+            <div className={s.starterImage}>
+              <div className={s.imageWrapper}>
+                <DatoImage
+                  className={s.test}
+                  data={page.starterImage.responsiveImage}
+                  pictureStyle={{
+                    objectFit: 'contain',
+                    objectPosition: 'left',
+                  }}
+                />
+              </div>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </TitleStripWithContent>
 
       <div className={s.features}>
-        <h2>{page.featuresTitle}</h2>
+        <Wrapper>
+          <div className={s.baloons}>
+            <div className={s.dev}>
+              <p>
+                <strong>
+                  If you’re a <Highlight style="good">dev</Highlight> 
+                </strong>{' '}
+                looking to hook up DatoCMS to your repos, head on over to <Link href={'/docs'}>our
+                docs</Link>
+                {/* <Button as="a" p="tiny" s="invert" href="/docs">
+                  To the docs
+                </Button> */}
+              </p>
+            </div>
+
+            <div className={s.editor}>
+              <p>
+                <strong>
+                  If you’re an{' '}
+                  <Highlight style="good">
+                    editor, marketer, or content creator 
+                  </Highlight>
+                </strong>{' '}
+                on the fence about whether or not DatoCMS is for you, you’re
+                probably…
+              </p>
+              <Arrow />
+            </div>
+          </div>
+        </Wrapper>
+
+        <Wrapper>
+          <div className={s.niceBlock}>
+            <div className={s.blockIllustration}>
+              <Zen />
+            </div>
+            <div className={s.blockText}>
+              <h2>{page.featuresTitle}</h2>
+              <StructuredText data={page.featuresDescription} />
+            </div>
+          </div>
+        </Wrapper>
+
         {page.callout.map((callout, index) => (
           <section
             key={index}
@@ -251,31 +304,42 @@ export default function UseCase({ subscription, preview }) {
         ))}
       </div>
 
-      <div className={s.successStory}>
-        <Wrapper>
+      <TitleStripWithContent
+        title={<StructuredText data={page.successStoryTitle} />}
+      >
+        <div className={s.successStory}>
           <div className={s.successStoryText}>
             <h2>
               <StructuredText data={page.successStoryTitle} />
             </h2>
             <StructuredText data={page.successStorySummary} />
-            <Button as="a" p="small" href={`/customers/${page.caseStudy.slug}`}>
-              Check it out
-            </Button>
+            <Space top={1}>
+              <Button
+                as="a"
+                p="small"
+                href={`/customers/${page.caseStudy.slug}`}
+              >
+                Check it out
+              </Button>
+            </Space>
           </div>
-        </Wrapper>
 
-        {page.successStoryImage?.responsiveImage && (
-          <div className={s.successStoryImage}>
-            <div className={s.imageWrapper}>
-              <DatoImage
-                className={s.test}
-                data={page.successStoryImage.responsiveImage}
-                pictureStyle={{ objectFit: 'contain', objectPosition: 'left' }}
-              />
+          {page.successStoryImage?.responsiveImage && (
+            <div className={s.successStoryImage}>
+              <div className={s.imageWrapper}>
+                <DatoImage
+                  className={s.test}
+                  data={page.successStoryImage.responsiveImage}
+                  pictureStyle={{
+                    objectFit: 'contain',
+                    objectPosition: 'left',
+                  }}
+                />
+              </div>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </TitleStripWithContent>
     </Layout>
   );
 }
