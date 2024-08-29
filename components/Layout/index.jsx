1e39c968bec9e336ddb1f6071f541e1af9ba2056
Marco Mezzavilla <hub@marcomezzavilla.com>
2024-08-29 11:59:33 +0200

    Clean up

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,14 +1,11 @@
 import BaseLayout from 'components/BaseLayout';
 import Footer from 'components/Footer';
-import MobileNavbar from 'components/MobileNavbar';
 import NewNavbar from 'components/NewNavbar';
 
 export default function Layout({ noCta, children, preview }) {
   return (
     <BaseLayout preview={preview}>
       <NewNavbar />
-      {/* <MobileNavbar /> */}
-
       {children}
       <Footer noCta={noCta} />
     </BaseLayout>
