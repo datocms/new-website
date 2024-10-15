a977a5b45d7c99cbdb8c15d76b258cd71b0b73c5
Ronak Ganatra <20298819+ronakganatra@users.noreply.github.com>
2024-10-15 12:03:37 +0200

    Update index.jsx

--- pages/legal/index.jsx
+++ pages/legal/index.jsx
@@ -26,27 +26,27 @@ export default function LegalPage({ body }) {
             The following pages cover our security and policies:
           </h3>
           <ul>
-            <li>
-              <Link href="/legal/privacy-policy" rel="noopener noreferrer">
-                Privacy Policy
-              </Link>
-            </li>
-            <li>
-              <Link href="/legal/cookie-policy" rel="noopener noreferrer">
-                Cookie Policy
-              </Link>
-            </li>
-            <li>
-              <Link href="/legal/gdpr" rel="noopener noreferrer">
-                GDPR Compliance
-              </Link>
-            </li>
-            <li>
-              <Link href="/legal/terms" rel="noopener noreferrer">
-                Terms of Service
-              </Link>
-            </li>
-          </ul>
+        <li>
+        <strong><Link href="/legal/privacy-policy" rel="noopener noreferrer">
+            Privacy Policy
+          </Link></strong>: <span>How we collect and process your data.</span>
+        </li>
+        <li>
+          <strong><Link href="/legal/cookie-policy" rel="noopener noreferrer">
+            Cookie Policy
+          </Link></strong>: <span>Understand our use of cookies and tracking.</span>
+        </li>
+        <li>
+        <strong><Link href="/legal/gdpr" rel="noopener noreferrer">
+            GDPR Compliance
+          </Link></strong>: <span>Information on how we handle personally identifiable data.</span>
+        </li>
+        <li>
+        <strong><Link href="/legal/terms" rel="noopener noreferrer">
+            Terms of Service
+          </Link></strong>: <span>The terms and conditions for using DatoCMS.</span>
+        </li>
+      </ul>
           </div>
       </Legal>
     </Layout>
